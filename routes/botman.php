4fbacf30c3bcfd0f90ab4085737bdc6beffc1d15
Mark Huijser <mhuijser@gmail.com>
2017-10-20 22:28:15 +0200

    Installing botman studio, based on laravel 5.5

---
+++ routes/botman.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
