6b4ae57a9616f6b29fde1c02915822e253c3f87b
Ronald Elzen <ronald_elzen@hotmail.com>
2017-10-21 18:28:13 +0200

    Working connection with  dialogflow
    Works but code is sloppy.

--- app/InputHandler.php
+++ app/InputHandler.php
@@ -10,8 +10,8 @@ namespace App;
 
 
 class InputHandler {
-	const INPUT_TYPE_SEARCH = 1; //Zoeken naar bepaalde term door hele bijbel
-	const INPUT_TYPE_LOOKUP = 2; //Bijbelgedeelte opvraag
+	const INPUT_TYPE_SEARCH = 'search'; //Zoeken naar bepaalde term door hele bijbel
+	const INPUT_TYPE_LOOKUP = 'lookup'; //Bijbelgedeelte opvraag
 	const INPUT_TYPE_SEARCH_AMOUNT = 3; //Aantal hit van bepaalde zoekterm door hele bijbel
 
 	private $rawInput;
@@ -22,20 +22,40 @@ class InputHandler {
 	private $parsedEndVerse;
 	private $apiName;
 
-	public function __construct( $rawInput ) {
-		$this->parseInput( $rawInput );
+	public function __construct() {
+
 	}
 
 	public function parseInput( $rawInput ) {
-		list( $bookName, $bookPart ) = explode( ' ', $rawInput );
-		list( $this->parsedChapter, $this->parsedBeginVerse ) = explode( ':' , $bookPart );
-		$this->parsedBookName = strtolower( $bookName );
-		$this->setApiName();
-//		if($rawInput == 'wat staat er in ...'){
-//			$this->inputType = self::INPUT_TYPE_LOOKUP;
-//		}elseif($rawInput == 'zoek naar ...'){
-//			$this->inputType = self::INPUT_TYPE_SEARCH;
-//		}
+		$curl = curl_init();
+		curl_setopt_array( $curl, array(
+			CURLOPT_URL            => "https://api.dialogflow.com/v1/query?v=20170712&query=" . urlencode( $rawInput ) . "&lang=en&sessionId=5676f59a-36d2-4106-bfa0-32ec0e993067&timezone=Europe%2FParis'",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING       => "",
+			CURLOPT_MAXREDIRS      => 10,
+			CURLOPT_TIMEOUT        => 30,
+			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST  => "GET",
+			CURLOPT_HTTPHEADER     => array(
+				"authorization: Bearer be3b238fa0f64f59b214990a2168ee4a",
+				"cache-control: no-cache",
+				"postman-token: 334a5136-62b6-d743-956f-62dc271530de"
+			),
+		) );
+		$response = curl_exec( $curl );
+		$err      = curl_error( $curl );
+		curl_close( $curl );
+		$result = json_decode( $response );
+		if($result->result->fulfillment->speech != ""){
+			return $result->result->fulfillment->speech;
+		}
+		else{
+			return array(
+			"book" => $result->result->parameters->Book,
+			"chapter" => $result->result->parameters->Chapter,
+			"verse" => $result->result->parameters->Verse,
+			);
+		}
 	}
 
 	/**
@@ -66,21 +86,14 @@ class InputHandler {
 		return $this->parsedEndVerse;
 	}
 
-	/**
-	 * @return mixed
-	 */
 	public function getApiName() {
-		return $this->apiName;
-	}
-
-	public function setApiName() {
 		$jsonString   = file_get_contents( '../data/book-mappings.json' );
 		$bookMappings = json_decode( $jsonString );
 		foreach ( $bookMappings as $bookMapping ) {
 			// $bot->reply(print_r($bookMapping, true));
-			if ( strtolower( $bookMapping->name ) == $this->parsedBookName || in_array( $this->parsedBookName, $bookMapping->alternatives )) {
+			if ( strtolower( $bookMapping->name ) == $this->parsedBookName || in_array( $this->parsedBookName, $bookMapping->alternatives ) ) {
 				$this->apiName = $bookMapping->apiName;
-				break;
+				return $this->apiName;
 			}
 		}
 	}
--- routes/botman.php
+++ routes/botman.php
@@ -1,25 +1,96 @@
 <?php
+
 use App\ApiHandler;
 use App\BibleVerses;
 use App\Http\Controllers\BotManController;
 use App\InputHandler;
+
 $botman = resolve( 'botman' );
 $botman->fallback( function ( $bot ) {
-	$bot->reply( 'Wat bedoel je? Vraag mij bijvoorbeeld \'Wat staat er in Johannes 3:16\'' );
+	$bot->reply( "Er is iets mis gegaan, probeer het later nog eens." );
 } );
-$botman->hears( '([.*])', function ( $bot, $passage ) {
-	$bot->reply( 'Gevonden gedeelte in de input: ' . $passage );
-} );
-$botman->hears( 'Wat staat er in {gedeelte}', function ( $bot, $input ) {
+
+$botman->hears( '{input}', function ( $bot, $input ) {
 	try {
-		$inputHandler = new InputHandler( $input );
-		$apiHandler   = new ApiHandler();
-		$bibleVerse = new BibleVerses($inputHandler->getParsedBeginVerse(), "", $inputHandler->getParsedBookName(), $inputHandler->getParsedChapter(), $inputHandler->getApiName());
-		$bibleVerse->setVerseText($apiHandler->sendApiCall( $bibleVerse->getApiName(), $bibleVerse->getChapter(), $bibleVerse->getBeginVerse(), "" ) );
-		$bot->reply( $bibleVerse->getVerseText() );
+		$Bijbelboek   = array(
+			"Genesis"           => "GEN",
+			"Exodus"            => "EXO",
+			"Leviticus"         => "LEV",
+			"Numeri"            => "NUM",
+			"Deutronomium"      => "DEU",
+			"Jozua"             => "JOS",
+			"Richteren"         => "JDG",
+			"Ruth"              => "RUT",
+			"1 Samuël"          => "1SA",
+			"2 Samuël"          => "2SA",
+			"1 Koningen"        => "1KI",
+			"2 Koningen"        => "2KI",
+			"1 Kronieken"       => "1CH",
+			"2 Kronieken"       => "2CH",
+			"Ezra"              => "EZR",
+			"Nehemia"           => "NEH",
+			"Esther"            => "EST",
+			"Job"               => "JOB",
+			"Psalmen"           => "PSA",
+			"Spreuken"          => "PRO",
+			"Prediker"          => "ECC",
+			"Hooglied"          => "SNG",
+			"Jesaja"            => "ISA",
+			"Jeremia"           => "JER",
+			"Klaagliederen"     => "LAM",
+			"Ezechiël"          => "EZK",
+			"Daniël"            => "DAN",
+			"Hosea"             => "HOS",
+			"Joël"              => "JOL",
+			"Amos"              => "AMO",
+			"Obadja"            => "OBA",
+			"Jona"              => "JON",
+			"Micha"             => "MIC",
+			"Nahum"             => "NAM",
+			"Habakuk"           => "HAB",
+			"Sefanja"           => "ZEP",
+			"Haggaï"            => "HAG",
+			"Zacharia"          => "ZEC",
+			"Maleachi"          => "MAL",
+			"Mattheüs"          => "MAT",
+			"Markus"            => "MRK",
+			"Lukas"             => "LUK",
+			"Johannes"          => "JHN",
+			"Handelingen"       => "ACT",
+			"Romeinen"          => "ROM",
+			"1 Korinthiërs"     => "1CO",
+			"2 Korinthiërs"     => "2CO",
+			"Galaten"           => "GAL",
+			"Efeziërs"          => "EPH",
+			"Filippenzen"       => "PHP",
+			"Kolossenzen"       => "COL",
+			"1 Tessalonicenzen" => "1TH",
+			"2 Tessalonicenzen" => "2TH",
+			"1 Timotheüs"       => "1TI",
+			"2 Timotheüs"       => "2TI",
+			"Titus"             => "TIT",
+			"Filemon"           => "PHM",
+			"Hebreeërs"         => "HEB",
+			"Jakobus"           => "JAS",
+			"1 Petrus"          => "1PE",
+			"2 Petrus"          => "2PE",
+			"1 Johannes"        => "1JN",
+			"2 Johannes"        => "2JN",
+			"3 Johannes"        => "3JN",
+			"Judas"             => "JUD",
+			"Openbaringen"      => "REV"
+		);
+		$inputHandler = new InputHandler();
+		$parsedInput  = $inputHandler->parseInput( $input );
+		if ( ! is_array( $parsedInput ) ) {
+			$bot->reply( $parsedInput );
+		} else {
+			$apiHandler = new ApiHandler();
+			$bot->reply( $apiHandler->sendApiCall( $Bijbelboek[ $parsedInput["book"] ], $parsedInput["chapter"], $parsedInput["verse"], "" ) );
+		}
 	}
-	catch(Exception $e){
-		$bot->reply('fout! ' . $e->getMessage());
+	catch ( Exception $e ) {
+		$bot->reply( $e->getMessage() );
 	}
-});
+} );
 $botman->hears( 'Start conversation', BotManController::class . '@startConversation' );
