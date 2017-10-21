9f40f0489198de4eef877555a8cded61858319ca
Ronald Elzen <ronald_elzen@hotmail.com>
2017-10-21 16:22:21 +0200

    Botman.php is back
    Like it never left.

--- routes/botman.php
+++ routes/botman.php
@@ -1,55 +1,25 @@
 <?php
+use App\ApiHandler;
+use App\BibleVerses;
 use App\Http\Controllers\BotManController;
-
-$botman = resolve('botman');
-
-$botman->fallback(function($bot) {
-    $bot->reply('Wat bedoel je? Vraag mij bijvoorbeeld \'wat staat er in Johannes 3:16\'');
-});
-
-$botman->hears('([.*])', function ($bot, $passage) {
-    $bot->reply('Gevonden gedeelte in de input: ' . $passage);
-});
-
-
-//wat staat er in Johannes 3:16
-$botman->hears('Wat staat er in {gedeelte}', function ($bot, $gedeelteString) {
-    //Gedeelte parsen naar API values
-    $chapter = '';
-    $verse = '';
-    list($bookName, $gedeelte) = explode(' ', $gedeelteString, 2);
-    list($chapter, $verse) = explode(':', $gedeelte);
-    $bookName = strtolower($bookName);
-    try{
-    	$jsonString = file_get_contents('../data/book-mappings.json');
-    	$bookMappings = json_decode($jsonString);
-    	foreach($bookMappings as $bookMapping){
-    		// $bot->reply(print_r($bookMapping, true));
-    		if(strtolower($bookMapping->name) == $bookName || in_array($bookName, $bookMapping->alternatives)){
-    			// $bot->reply('Mapping gevonden: ' . print_r($bookMapping, true));
-    			$apiResponse = file_get_contents('https://bijbel.eo.nl/api/' . $bookMapping->apiName . '/' . $chapter . '/' . $verse);
-    			$apiResponse = str_replace(array('/**/callback(', ');'), '', $apiResponse);
-    			$resultObject = json_decode($apiResponse);
-    			$responseString = $resultObject[0]->_source->flat_content;
-    		}
-    	}
-    }
-    catch(Exception $e){
-    	$bot->reply('Oops, something went wrong: ' . $e->getMessage());
-    }
-
-    //API call doen
-    
-
-    //API response parsen
-    if(isset($responseString)){
-    	$bot->reply('In ' . $bookName . ' ' . $chapter . ' vers ' . $verse . ' staat \'' . $responseString . '\'');
+use App\InputHandler;
+$botman = resolve( 'botman' );
+$botman->fallback( function ( $bot ) {
+	$bot->reply( 'Wat bedoel je? Vraag mij bijvoorbeeld \'Wat staat er in Johannes 3:16\'' );
+} );
+$botman->hears( '([.*])', function ( $bot, $passage ) {
+	$bot->reply( 'Gevonden gedeelte in de input: ' . $passage );
+} );
+$botman->hears( 'Wat staat er in {gedeelte}', function ( $bot, $input ) {
+	try {
+		$inputHandler = new InputHandler( $input );
+		$apiHandler   = new ApiHandler();
+		$bibleVerse = new BibleVerses($inputHandler->getParsedBeginVerse(), "", $inputHandler->getParsedBookName(), $inputHandler->getParsedChapter(), $inputHandler->getApiName());
+		$bibleVerse->setVerseText($apiHandler->sendApiCall( $bibleVerse->getApiName(), $bibleVerse->getChapter(), $bibleVerse->getBeginVerse(), "" ) );
+		$bot->reply( $bibleVerse->getVerseText() );
 	}
-	else {
-    	// $bot->reply('Het gevraagd gedeelte is ' . $gedeelteString . '. Boek: ' . $bookName . ', gedeelte: ' . $gedeelte);
-    	$bot->reply('Sorry, hier kon ik niets mee');
-    }
-
+	catch(Exception $e){
+		$bot->reply('fout! ' . $e->getMessage());
+	}
 });
-
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+$botman->hears( 'Start conversation', BotManController::class . '@startConversation' );
