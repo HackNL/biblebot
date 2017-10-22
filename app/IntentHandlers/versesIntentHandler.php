65141e3d6269359d98492880b3b6437ab52ea87b
Herman Kremer <herman.kremer@eo.nl>
2017-10-22 11:47:28 +0200

    add header to verse output. And fix return only one verse is one verse is requseted

--- app/IntentHandlers/versesIntentHandler.php
+++ app/IntentHandlers/versesIntentHandler.php
@@ -1,14 +1,32 @@
 <?php
+
 namespace App\IntentHandlers;
 
 use App\BookMapper;
 use App\ApiHandler;
 
-class versesIntentHandler extends baseIntentHandler {
+class versesIntentHandler extends baseIntentHandler
+{
 
-	/** API call to EO */
-	public function handle(){
-		$apiHandler = new ApiHandler();
-		return $apiHandler->sendApiCall( BookMapper::getApiNameByName($this->parameters["Book"]), $this->parameters["Chapter"], $this->parameters["Verse"], $this->parameters['VerseEnd'] );
-	}
+    /** API call to EO */
+    public function handle()
+    {
+        $apiHandler = new ApiHandler();
+        $header = $this->parameters["Book"] . ' ';
+        $header .= $this->parameters["Chapter"] . ' vers ';
+        $header .= $this->parameters["Verse"] . ' ';
+        if ($this->parameters['VerseEnd'])
+        {
+            $header .= 'tot ' . $this->parameters['VerseEnd'];
+        };
+        return [
+            $header,
+            $apiHandler->sendApiCall(
+                BookMapper::getApiNameByName($this->parameters["Book"]),
+                $this->parameters["Chapter"],
+                $this->parameters["Verse"],
+                $this->parameters['VerseEnd']
+            )
+        ];
+    }
 }
